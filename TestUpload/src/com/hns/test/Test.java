9424ea7759a0967c99ac68ba4938a94d290f5998
mchaabani <mchaabani@16STALA025-iaul.groupehn.com>
2021-12-24 16:52:56 +0100

    Commit From PC HNS

--- TestUpload/src/com/hns/test/Test.java
+++ TestUpload/src/com/hns/test/Test.java
@@ -3,7 +3,6 @@ package com.hns.test;
 public class Test {
   public static void main(String[] args) {
 	System.out.println("Test Mch....");      
-	//From PC de Mahdi
-	//From PC de HNS
+    //PC HNS
 }
 } 
