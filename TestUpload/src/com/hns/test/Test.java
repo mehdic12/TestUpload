e1a4ca0ca9643abf0d2836565389b37dbf403b61
mchaabani <mchaabani@16STALA025-iaul.groupehn.com>
2021-12-24 16:10:23 +0100

    Test-MCH

--- TestUpload/src/com/hns/test/Test.java
+++ TestUpload/src/com/hns/test/Test.java
@@ -2,6 +2,6 @@ package com.hns.test;
 
 public class Test {
   public static void main(String[] args) {
-	System.out.println("Test Mch....");       
+	System.out.println("Test Mch....");         
 }
 } 
