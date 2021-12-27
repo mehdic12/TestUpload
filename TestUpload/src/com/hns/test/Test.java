b3f7f0ad8e815a0fde3078a6779890cb8493d1a6
mchaabani <mchaabani@16STALA025-iaul.groupehn.com>
2021-12-27 15:31:52 +0100

    Commit in banche 1.0.1

--- TestUpload/src/com/hns/test/Test.java
+++ TestUpload/src/com/hns/test/Test.java
@@ -3,6 +3,6 @@ package com.hns.test;
 public class Test {
   public static void main(String[] args) {
 	System.out.println("Test Mch....");      
-    //PC HN Services.. Commit N 
+    //Commit in branche 1.0.1
 }
 } 
