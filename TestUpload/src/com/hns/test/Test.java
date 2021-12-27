48397bb4687d901d439d53732140e1b07e031d8e
mchaabani <mchaabani@16STALA025-iaul.groupehn.com>
2021-12-27 10:10:18 +0100

    Test Commit Aujour

--- TestUpload/src/com/hns/test/Test.java
+++ TestUpload/src/com/hns/test/Test.java
@@ -3,6 +3,6 @@ package com.hns.test;
 public class Test {
   public static void main(String[] args) {
 	System.out.println("Test Mch....");      
-    //PC HN Services...
+    //PC HN Services.. Test Aujourd ui
 }
 } 
