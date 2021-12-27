eb29bd8ebede0844a7954c0279e0358ffeed0381
mchaabani <mchaabani@16STALA025-iaul.groupehn.com>
2021-12-27 15:53:04 +0100

    test

--- TestUpload/src/com/hns/test/Test.java
+++ TestUpload/src/com/hns/test/Test.java
@@ -2,7 +2,7 @@ package com.hns.test;
 
 public class Test {
   public static void main(String[] args) {
-	System.out.println("Test Mch....");   
-	//Branche master 
+	System.out.println("Test Mch....");      
+    //Commit in branche 1.0.1
 }
 } 
