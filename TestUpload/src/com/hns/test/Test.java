6e77439e0fe1405314969624f97bfcfa3894eee8
mchaabani <mchaabani@16STALA025-iaul.groupehn.com>
2022-01-07 11:31:38 +0100

    Test MCH

--- TestUpload/src/com/hns/test/Test.java
+++ TestUpload/src/com/hns/test/Test.java
@@ -3,6 +3,6 @@ package com.hns.test;
 public class Test {
   public static void main(String[] args) {
 	System.out.println("Test Mch....");      
-    //Commit in branche 1.0.1
+    //Commit in branche 1.0.1  
 }
 } 
