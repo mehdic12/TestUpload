816b5a1685bcbec126142262b38af38323643281
mchaabani <mchaabani@16STALA025-iaul.groupehn.com>
2022-01-07 17:22:39 +0100

    Nouvelle Modification....

--- TestUpload/src/com/hns/test/Test.java
+++ TestUpload/src/com/hns/test/Test.java
@@ -2,7 +2,7 @@ package com.hns.test;
 
 public class Test {
   public static void main(String[] args) {
-	System.out.println("Test Mch....");  //Test Jenkins Integration...    
+	System.out.println("Test Mch....");  //Nouvelle Modification.... 
  
 }
 } 
